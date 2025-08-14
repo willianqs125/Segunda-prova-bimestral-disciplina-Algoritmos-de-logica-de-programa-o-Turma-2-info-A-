1bbd0d31b58ef2ab895e9ca1094393e2987d92a1
willianqs125 <willianrodriguesdequadros@gmail.com>
2025-08-14 11:26:03 -0300

    Update Questão 5.c

--- Questão 5.c
+++ Questão 5.c
@@ -2,13 +2,11 @@
 
 int main() {
     int numero;
-
-
+    
     printf("Digite um número inteiro: ");
     scanf("%d", &numero);
 
-    
-    if ((numero % 2 == 0 && numero % 3 != 0) || (numero % 3 == 0 && numero % 2 != 0)) {
+    if ((numero % 2 == 0) != (numero % 3 == 0)) {
         printf("O número %d é divisível por 2 ou por 3, mas não por ambos.\n", numero);
     } else {
         printf("O número %d não atende à condição (divisível apenas por 2 ou apenas por 3).\n", numero);
