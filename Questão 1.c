6ef2b42c6951ff92837ce5d22cb21c46f05bb967
willianqs125 <willianrodriguesdequadros@gmail.com>
2025-08-14 11:08:23 -0300

    Update Questão 1.c

--- Questão 1.c
+++ Questão 1.c
@@ -3,11 +3,11 @@
 int main() {
     int numero;
 
-    // Solicita o número ao usuário
+    
     printf("Digite um numero de cinco digitos: ");
     scanf("%d", &numero);
 
-    // Verifica se o número tem exatamente cinco dígitos
+    
     if (numero < 10000 || numero > 99999) {
         printf("Entrada invalida. Digite um numero com exatamente cinco digitos.\n");
         return 1;
@@ -16,7 +16,7 @@ int main() {
     int d2 = (numero / 1000) % 10;
     int d3 = (numero / 100) % 10;
     int d4 = (numero / 10) % 10;
-    int d5 = numero% 10;
+    int d5 = numero % 10;
     
     printf("%d   %d   %d   %d   %d\n", d1, d2, d3, d4, d5);
 
