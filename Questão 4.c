823fff414dc33e500feac66f023e6c334854545f
willianqs125 <willianrodriguesdequadros@gmail.com>
2025-08-14 10:50:51 -0300

    Create Questão 4.c

---
+++ Questão 4.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+
+int main() {
+    int opcao;
+    double num1, num2, resultado;
+
+    
+    printf("Escolha uma operação:\n");
+    printf("1 - Soma\n");
+    printf("2 - Subtração\n");
+    printf("3 - Multiplicação\n");
+    printf("4 - Divisão\n");
+    printf("Digite a opção desejada (1-4): ");
+    scanf("%d", &opcao);
+
+    
+    printf("Digite o primeiro número: ");
+    scanf("%lf", &num1);
+
+    printf("Digite o segundo número: ");
+    scanf("%lf", &num2);
+
+    switch (opcao) {
+        case 1:
+            resultado = num1 + num2;
+            printf("Resultado da soma: %.2lf\n", resultado);
+            break;
+        case 2:
+            resultado = num1 - num2;
+            printf("Resultado da subtração: %.2lf\n", resultado);
+            break;
+        case 3:
+            resultado = num1 * num2;
+            printf("Resultado da multiplicação: %.2lf\n", resultado);
+            break;
+        case 4:
+            
+            if (num2 != 0) {
+                resultado = num1 / num2;
+                printf("Resultado da divisão: %.2lf\n", resultado);
+            } else {
+                printf("Erro: Não é possível dividir por zero!\n");
+            }
+            break;
+        default:
+            printf("Opção inválida! Por favor, escolha uma opção entre 1 e 4.\n");
+            break;
+    }
+
+    return 0;
+}
