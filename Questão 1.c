4e3aead0a8021e2d146174ef3f086494528fb4a0
willianqs125 <willianrodriguesdequadros@gmail.com>
2025-08-15 09:19:34 -0300

    Update Questão 1.c

--- Questão 1.c
+++ Questão 1.c
@@ -1,24 +1,24 @@
 #include <stdio.h>
 
 int main() {
-    int numero;
+    int numero1;
 
     
     printf("Digite um numero de cinco digitos: ");
-    scanf("%d", &numero);
+    scanf("%d", &numero1);
 
     
-    if (numero < 10000 || numero > 99999) {
+    if (numero1 < 10000 || numero1 > 99999) {
         printf("Entrada invalida. Digite um numero com exatamente cinco digitos.\n");
         return 1;
     }
-    int d1 = numero / 10000;
-    int d2 = (numero / 1000) % 10;
-    int d3 = (numero / 100) % 10;
-    int d4 = (numero / 10) % 10;
-    int d5 = numero % 10;
+    int n1 = numero1 / 10000;
+    int n2 = (numero1 / 1000) % 10;
+    int n3 = (numero1 / 100) % 10;
+    int n4 = (numero1 / 10) % 10;
+    int n5 = numero1 % 10;
     
-    printf("%d   %d   %d   %d   %d\n", d1, d2, d3, d4, d5);
+    printf("%d   %d   %d   %d   %d\n", n1, n2, n3, n4, n5);
 
     return 0;
 }
